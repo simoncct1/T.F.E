993abd1a808b3020aa25cf63a1bd7715d1ff0763
Simon Couclet <96738359+simoncct1@users.noreply.github.com>
2022-05-05 14:30:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from "../node_modules/three/build/three.module.js;
+import * as THREE from "../node_modules/three/build/three.module.js";
 import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from '../node_modules/examples/jsm/loaders/GLTFLoader.js';
 import { Reflector } from '../node_modules/examples/jsm/objects/Reflector';
